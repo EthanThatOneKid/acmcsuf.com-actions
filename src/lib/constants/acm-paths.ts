e84518b4109869c70b4fd35b59cca52c2d279b62
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2022-08-24 19:20:45 -0700

    Initial commit
    Based on https://github.com/EthanThatOneKid/acmcsuf.com/commit/d8ca7f4756b82712e638847373b75e01bd64c466

---
+++ src/lib/constants/acm-paths.ts
@@ -0,0 +1,39 @@
+export interface AcmPath {
+  title: string;
+  slug: string;
+  picture: string;
+  color: string;
+}
+
+export const acmAlgo: AcmPath = {
+  title: 'Algo',
+  slug: 'algo',
+  picture: './assets/badges/algo-team.svg',
+  color: 'var(--acm-purple)',
+};
+
+export const acmCreate: AcmPath = {
+  title: 'Create',
+  slug: 'create',
+  picture: './assets/badges/design-team.svg',
+  color: 'var(--acm-pink)',
+};
+
+export const acmDev: AcmPath = {
+  title: 'Dev',
+  slug: 'dev',
+  picture: './assets/badges/dev-team.svg',
+  color: 'var(--acm-bluer)',
+};
+
+export const acmGeneral: AcmPath = {
+  title: 'General',
+  slug: 'general',
+  picture: './assets/badges/general.svg',
+  color: 'var(--acm-blue)',
+};
+
+export const acmPaths = [acmAlgo, acmCreate, acmDev, acmGeneral] as const;
+
+/** Pinned paths are featured on the landing page. */
+export const pinnedPaths = [acmAlgo, acmCreate, acmDev] as const;
---
+++ src/lib/constants/index.ts
@@ -0,0 +1,8 @@
+export * from './acm-paths';
+export * from './links';
+export * from './officers';
+
+// DEBUG flag is activated when anything besides '', '0', or 'false' is selected
+export const DEBUG = !['', '0', 'false', 'undefined'].includes(
+  String(import.meta.env.VITE_DEBUG).toLowerCase()
+);
